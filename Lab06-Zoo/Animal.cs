cdd671d5093a37ab7b7953a6457bd82560d1b6a7
cnickels21 <cnickels_21@yahoo.com>
2020-05-11 18:07:29 -0500

    animal base class defined

---
+++ Lab06-Zoo/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06_Zoo
+{
+    abstract class Animal
+    {
+        /// <summary>
+        /// Virtual property to be overridden in subsequent classes
+        /// </summary>
+        public virtual int NumberInPen { get; set; } = 2;
+
+        /// <summary>
+        /// Abstract methods to be overridden on next or subsequent layers
+        /// </summary>
+        public abstract void Verb();
+        public abstract void Sound();
+
+        /// <summary>
+        /// Virtual method to potentially be overridden down the line
+        /// </summary>
+        public virtual void Sleep()
+        {
+            Console.WriteLine("This animal sleeps at night.");
+        }
+
+
+    }
+}
