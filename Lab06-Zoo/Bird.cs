56a81a79d3b01a0235de8521cb5bdbe778016dca
cnickels21 <cnickels_21@yahoo.com>
2020-05-11 18:39:43 -0500

    peacock concrete class defined

--- Lab06-Zoo/Bird.cs
+++ Lab06-Zoo/Bird.cs
@@ -1,7 +1,20 @@
-﻿namespace Lab06_Zoo
+﻿using System;
+
+namespace Lab06_Zoo
 {
     public abstract class Bird : Animal
     {
+        /// <summary>
+        /// First of two required abstract properties written into the hierarchy of my classes
+        /// </summary>
+        public abstract string Color { get; set; }
 
+        /// <summary>
+        /// Virtual method for birds here
+        /// </summary>
+        public virtual void Fly()
+        {
+            Console.WriteLine("All of our birds know how to fly, but we will have penguins soon!");
+        }
     }
 }
---
+++ Lab06-Zoo/Peacock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab06_Zoo
+{
+    /// <summary>
+    /// Concrete animal derivative class 1 of 5
+    /// </summary>
+    public class Peacock : Bird
+    {
+        private const string peacockColor = "Our peacocks are albino!";
+
+        public override string Color { set => peacockColor; }
+
+        public override void Action()
+        {
+            Console.WriteLine("Both peacock charges as you approach!");
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("The peacock sounds like it's laughing as you run away scared.");
+        }
+    }
+}
